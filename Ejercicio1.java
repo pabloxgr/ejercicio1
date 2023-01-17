48e5f38d3874fc6a1a4e90f62907f9eaaa0169bd
Juan Pablo <pabloguzmanr05@gmail.com>
2023-01-17 11:01:57 -0500

    Add two activities and desing the connection between then

---
+++ Ejercicio1.java
@@ -0,0 +1,30 @@
+package com.ejercicios;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        int num1=1;
+        float num2=2.6f;
+        byte num4=5;
+        long num5=204;
+        double num3=4773.5;
+
+        char letra='A';
+        boolean comp1= 5<10;
+        boolean comp2= 9<8;
+
+        String nombre="Juan";
+
+
+        System.out.println("Los numero enteros son "+num1 + ", " + num4 + ", " + num5);
+        System.out.println("El numero decimal flotante es "+num2);
+        System.out.println("El numero decimal double es "+num3);
+        System.out.println("La primera letra del abecedario es la "+ letra);
+        System.out.println("La comparacion de 5 < 10 es "+ comp1);
+        System.out.println("La comparacion de 9<8 es "+comp2);
+        System.out.println("El nombre del aprendiz del curso de Java es "+ nombre);
+
+
+    }
+}
